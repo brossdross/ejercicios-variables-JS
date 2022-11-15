1e496dc016227dec502a4c2b1bc2be467c08017c
Maximiliano <djmaxirey@gmail.com>
2022-11-15 10:28:06 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,76 @@
+// 1) alert("¡Hola Mundo!")
+
+/* 2) let saludo = "¡Hola Mundo!"
+alert(saludo)
+console.log(saludo)
+ */
+
+
+/* 3) let entrada = prompt("Inrese su Nombre")
+let salida = "Hola" + " " + entrada
+alert(salida) */
+
+/* 4) let num1 = (3)
+let num2 = (2)
+let num3 = (5)
+
+let suma = (num1 + num2)
+let resta = (num2 - num3)
+let division = (suma / resta)
+let alCuadrado = (division * division)
+alert(alCuadrado)
+ */
+
+
+/* 5) let horas = prompt("¿Cuantas horas Trabajo?")
+let costo = prompt("¿Cuanto cobra usted por hora?")
+let salario = (horas * costo)
+alert("Su salario es $4" + salario) */
+
+
+/* 6) let peso = prompt("¿Cual es su peso?")
+let altura = prompt("¿Cuanto mides?")
+let alturaAlCuadrado = (altura * altura)
+let imc = (peso / alturaAlCuadrado)
+alert("Su IMC es: " + imc) */
+
+
+/* 7) let num1 = prompt("Ingrese un numero de su agrado para el dividendo")
+let num2 = prompt("Ingrese otro numero de su agrado para el divisor")
+let resultado = (num1 / num2)
+alert("El resultado de su division es " + resultado) */
+
+
+
+/* 8) let inversion = prompt("¿Cuanto desea invertir?")
+let interes = prompt("¿Que ineteres anual desearia tener?")
+let anios = prompt("¿A cuanto años quiere extender la inversion?")
+let interesAcumulado = (inversion * interes)
+let interesFinal = (interesAcumulado + anios)
+alert("Al finalizar el plazo usted tendria: " + interesFinal)
+ */
+
+
+
+/* 9) let payasos = prompt("¿Cuantos payasos se deben enviar en el pedido?")
+let muniecas = prompt("¿Cuantas muñecas se deben enviar en el pedido?")
+let pesoPayasos = (payasos * 0.112)
+let pesoMuniecas = (muniecas * 0.75)
+let pesoPaquete = (pesoPayasos + pesoMuniecas)
+alert("El peso del paquete a enviar es de " + pesoPaquete + "Kg")
+ */
+
+
+
+
+/* 10) */
+
+let cantidad = prompt("¿Cuantas barras de pan que no son del dia usted adquirio?")
+let precio = (3.49)
+let precioNormal = (cantidad * precio)
+let descuento = (precio * 0.60)
+let total = (descuento * cantidad)
+let final = ("El precio de su compra es " + precioNormal + " " + "Por no ser pan del dia se le realiza un descuento de " + descuento + " " + "El precio total seria " + total)
+alert(final)
+ 
+
